9a7e85a7270c9b6ea77f48e62554dd6d7d37d68a
Timothy Gibson <ryaninlux@gmail.com>
2022-03-25 11:49:05 -0700

    Not change much. WIP going to eat lunch

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
     $("form#triangle").submit(function(event) {
     event.preventDefault();
-    const question1Input = $("#question1").val()
-    const question2Input = $("#question2").val()
-    const question3Input = $("#question3").val()
-    const question4Input = $("#question4").val()
-    const question5Input = $("#question5").val()
+    const question1Input = $("select#question1").val()
+    const question2Input = $("select#question2").val()
+    const question3Input = $("select#question3").val()
+    const question4Input = $("select#question4").val()
+    const question5Input = $("select#question5").val()
+
+    $("button#message").click(function() {
+      $("p").prepend("<p>Python</p>");
+      $("#message").show();
+    });
     
   });
   });
