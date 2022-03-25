af62a0eefc907e780ff19b78da90463bedb03e39
Timothy Gibson <ryaninlux@gmail.com>
2022-03-25 11:06:17 -0700

    Add a message in html and display for message in css

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
     $("form#triangle").submit(function(event) {
     event.preventDefault();
-    const question1 = $("#question1").val()
-
+    const question1Input = $("#question1").val()
+    const question2Input = $("#question2").val()
+    const question3Input = $("#question3").val()
+    const question4Input = $("#question4").val()
+    const question5Input = $("#question5").val()
+    
   });
   });
