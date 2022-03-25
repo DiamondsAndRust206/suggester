cd24a63e1242421e16100fa9075b0245db692e9d
Timothy Gibson <ryaninlux@gmail.com>
2022-03-25 15:19:12 -0700

    Rephrased the questions for the input in html. Add js but not working. Also did some styling in css by changing the background and font colors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   $("form#triangle").submit(function(event) {
   event.preventDefault();
-
+  const firstQuestion = $("#question1").val();
+  const secondQuestion = $("#question2").val();
+  const thirdQuestion = $("#question3").val();
+  const fourthQuestion = $("#question4").val();
+  const fifthQuestion = $("#question5").val();
+  
+  if(firstQuestion === "coffee" && secondQuestion === "dogs" && thirdQuestion === "summer" && fourthQuestion === "heights" && fifthQuestion === "swimming") {
+    alert("python");
+  }
 });
 })
