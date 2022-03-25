9cf50345658396a23b9822331022977e4e9d0984
Timothy Gibson <ryaninlux@gmail.com>
2022-03-25 14:22:58 -0700

    Completely redid the html. Could not get the select boxes to work with javascript so I change them to input boxes. Hopefully I haven't wasted to much time

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
-    $("form#triangle").submit(function(event) {
-    event.preventDefault();
-    const question1Input = $("select#question1").val()
-    const question2Input = $("select#question2").val()
-    const question3Input = $("select#question3").val()
-    const question4Input = $("select#question4").val()
-    const question5Input = $("select#question5").val()
+  $("form#triangle").submit(function(event) {
+  event.preventDefault();
 
-    $("button#message").click(function() {
-      $("p").prepend("<p>Python</p>");
-      $("#message").show();
-    });
-    
-  });
-  });
+});
+})
